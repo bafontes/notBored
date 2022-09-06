7d05c4494088d94be17e71fab027d4677f731069
oroscoiaraML <iara.orosco@mercadolibre.com>
2022-09-06 13:28:23 -0300

    Update SceneDelegate.swift

--- NotBored/NotBored/NotBored/Common/SceneDelegate.swift
+++ NotBored/NotBored/NotBored/Common/SceneDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    
+    //prueba
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
         guard let windowScene = (scene as? UIWindowScene) else { return }
