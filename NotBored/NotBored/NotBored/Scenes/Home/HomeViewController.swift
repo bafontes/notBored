3ee5f598d63038b67e7e863ed6f288cf8c7ef295
Bruno Andres Fontes <bfontes@uy0fvfhl07sq05p.ml.com>
2022-09-06 12:57:24 -0300

    init project

---
+++ NotBored/NotBored/NotBored/Scenes/Home/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  NotBored
+//
+//  Created by Bruno Andres Fontes on 6/9/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
