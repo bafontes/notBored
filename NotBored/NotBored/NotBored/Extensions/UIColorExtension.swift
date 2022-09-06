c991a33e8c65f0644b3bf0be39a92a6fdbfd2544
Bruno Andres Fontes <bfontes@uy0fvfhl07sq05p.ml.com>
2022-09-06 15:04:33 -0300

    colors extension

---
+++ NotBored/NotBored/NotBored/Extensions/UIColorExtension.swift
@@ -0,0 +1,14 @@
+//
+//  UIColorExtension.swift
+//  NotBored
+//
+//  Created by Bruno Andres Fontes on 6/9/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let mainColor = UIColor(named: "main")!
+    static let backgroundColor = UIColor(named: "background")!
+}
