a16e655ccf73ba7cffcfe607387047f3bb542c73
Bruno Andres Fontes <bfontes@uy0fvfhl07sq05p.ml.com>
2022-09-08 10:58:43 -0300

    home

--- NotBored/NotBored/NotBored/Scenes/Home/HomeViewController.swift
+++ NotBored/NotBored/NotBored/Scenes/Home/HomeViewController.swift
@@ -9,10 +9,44 @@ import UIKit
 
 class HomeViewController: BaseViewController {
     
+    @IBOutlet weak var titleLabel: UILabel! {
+        didSet {
+            titleLabel.text = "Not Bored"
+            titleLabel.textColor = .mainColor
+            titleLabel.font = .systemFont(ofSize: 35, weight: .bold)
+        }
+    }
+    @IBOutlet weak var subtitleLabel: UILabel! {
+        didSet {
+            subtitleLabel.text = "Participants"
+            subtitleLabel.font = .systemFont(ofSize: 16, weight: .bold)
+        }
+    }
+    @IBOutlet weak var textfield: UITextField!
+    @IBOutlet weak var nextButton: UIButton! {
+        didSet {
+            nextButton.setTitle("Start", for: .normal)
+            nextButton.backgroundColor = .mainColor
+            nextButton.tintColor = .white
+            nextButton.layer.cornerRadius = 5
+        }
+    }
+    @IBOutlet weak var termsAndConditionsButton: UIButton! {
+        didSet {
+            termsAndConditionsButton.tintColor = .black
+            let attributes: [NSAttributedString.Key: Any] = [
+                .underlineStyle: NSUnderlineStyle.single.rawValue,
+                .font: UIFont.systemFont(ofSize: 16, weight: .medium),
+            ]
+            let attributeString = NSMutableAttributedString(string: "Terms and conditions", attributes: attributes)
+            termsAndConditionsButton.setAttributedTitle(attributeString, for: .normal)
+        }
+    }
+//    MARK: - life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.isNavigationBarHidden = true
-        title = "Home"
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -20,4 +54,22 @@ class HomeViewController: BaseViewController {
         navigationController?.isNavigationBarHidden = false
     }
     
+//    MARK: - actions
+    
+    @IBAction func nextButtonAction(_ sender: Any) {
+        guard let cantParticipants: Int = Int(textfield.text ?? "") else {
+            textfield.layer.borderColor = UIColor.red.cgColor
+            textfield.layer.borderWidth = 2
+            textfield.layer.cornerRadius = 10
+            textfield.textColor = .red
+            return
+        }
+        print(cantParticipants)
+        textfield.layer.borderWidth = 0
+        textfield.textColor = .black
+//        TODO: make redirection with cantParticipant
+    }
+    @IBAction func termsAndConditionsButtonAction(_ sender: Any) {
+//        TODO: make redirection to terms and conditions scene
+    }
 }
