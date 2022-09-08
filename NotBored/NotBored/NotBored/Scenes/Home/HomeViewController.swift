bfb80bd9b922b664e9cad75e43d636adf770c9f1
Bruno Andres Fontes <bfontes@uy0fvfhl07sq05p.ml.com>
2022-09-08 14:23:16 -0300

    home updated

--- NotBored/NotBored/NotBored/Scenes/Home/HomeViewController.swift
+++ NotBored/NotBored/NotBored/Scenes/Home/HomeViewController.swift
@@ -22,7 +22,11 @@ class HomeViewController: UIViewController {
             subtitleLabel.font = .systemFont(ofSize: 16, weight: .bold)
         }
     }
-    @IBOutlet weak var textfield: UITextField!
+    @IBOutlet weak var textfield: UITextField! {
+        didSet {
+            textfield.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        }
+    }
     @IBOutlet weak var nextButton: UIButton! {
         didSet {
             nextButton.setTitle("Start", for: .normal)
@@ -42,30 +46,73 @@ class HomeViewController: UIViewController {
             termsAndConditionsButton.setAttributedTitle(attributeString, for: .normal)
         }
     }
-//    MARK: - life cycle
+    @IBOutlet weak var picker: UIPickerView! {
+        didSet {
+            picker.dataSource = self
+            picker.delegate = self
+        }
+    }
+    
+    let pickerData: [PriceRanges] = [.free, .low, .medium, .high]
+    var rangeSelected: PriceRanges = .free
+    var cantParticipants: Int = 0
+    //    MARK: - life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .backgroundColor
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
 //    MARK: - actions
     
     @IBAction func nextButtonAction(_ sender: Any) {
-        guard let cantParticipants: Int = Int(textfield.text ?? "") else {
-            textfield.layer.borderColor = UIColor.red.cgColor
-            textfield.layer.borderWidth = 2
-            textfield.layer.cornerRadius = 10
-            textfield.textColor = .red
-            return
-        }
-        print(cantParticipants)
-        textfield.layer.borderWidth = 0
-        textfield.textColor = .black
-//        TODO: make redirection with cantParticipant
+        let vc: ActivitiesViewController = ActivitiesViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
     }
     @IBAction func termsAndConditionsButtonAction(_ sender: Any) {
         let vc: TermsAndConditionsViewController = TermsAndConditionsViewController()
         vc.modalPresentationStyle = .fullScreen
         self.navigationController?.present(vc, animated: true)
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        guard let cantParticipants: Int = Int(textfield.text ?? ""), cantParticipants > 0  else {
+            textfield.layer.borderColor = UIColor.red.cgColor
+            textfield.layer.borderWidth = 2
+            textfield.layer.cornerRadius = 10
+            textfield.textColor = .red
+            nextButton.isEnabled = false
+            nextButton.backgroundColor = .gray
+            return
+        }
+        nextButton.isEnabled = true
+        nextButton.backgroundColor = .mainColor
+        self.cantParticipants = cantParticipants
+        textfield.layer.borderWidth = 0
+        textfield.textColor = .black
+    }
+}
+extension HomeViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int { 1 }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        self.pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row].rawValue
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.rangeSelected = pickerData[row]
     }
 }
